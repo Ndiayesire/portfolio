2373b3c9cd315cdac0793b68ad564262db0043c7
Ndiaye Sire Kane <ndiayesirekane@gmail.com>
2024-03-07 17:27:09 +0000

    Fix path image

--- src/project.js
+++ src/project.js
@@ -1,30 +1,30 @@
 export const PROJECT = [
     {
         name: 'Online MarketPlace',
-        image: '/src/assets/4.png',
+        image: '/assets/4.png',
     },
     {
         name: 'Sama Kalpé',
-        image: '/src/assets/7.png',
+        image: '/assets/7.png',
     },
 
     {
         name: 'Bayre Host',
-        image: '/src/assets/1.png',
+        image: '/assets/1.png',
     },
 
     {
         name: 'Sentech Valley',
-        image: '/src/assets/2.png',
+        image: '/assets/2.png',
     },
 
     {
         name: 'Currency Converter',
-        image: '/src/assets/6.png',
+        image: '/assets/6.png',
     },
 
     {
         name: 'Todo APP',
-        image: '/src/assets/5.png',
+        image: '/assets/5.png',
     },
 ]
